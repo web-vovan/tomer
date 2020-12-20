81ba6cc2ef85ae5627c270a0fbcc88053541d688
vladimir <myweb11@mail.ru>
2020-12-20 18:54:40 +0300

    настроил таймеры

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,147 @@
 <template>
   <div id="app">
-    <table>
-      <tr>
-        <td>workTime</td>
-        <td>breakTime</td>
-      </tr>
-      <tr>
-        <td>{{ workTime }}</td>
-        <td>{{ breakTime }}</td>
-      </tr>
-    </table>
-    <button v-if="isPlay" v-on:click="countdown">stop</button>
-    <button v-else v-on:click="countdown">start</button>
+    <header>
+      <div class="logo"><img src="./assets/image/logo.svg" alt=""></div>
+      <h1>Tomer</h1>
+    </header>
+
+    <audio src="./assets/sounds/stopTimer.mp3"></audio>
+    <div class="timer-wrap">
+      <div
+          class="item"
+          data-timer="workTimer"
+          v-on:click="activate"
+          v-bind:class="{ active: activeTimer === 'workTimer' }"
+      >
+        {{ workTimer }}
+      </div>
+      <div
+          class="item"
+          data-timer="breakTimer"
+          v-on:click="activate"
+          v-bind:class="{ active: activeTimer === 'breakTimer' }">
+        {{ breakTimer }}
+      </div>
+    </div>
+    <div class="timer-button">
+      <img src="./assets/image/start.svg" v-on:click="startTimer" v-if="!isPlay">
+      <img src="./assets/image/stop.svg" v-on:click="stopTimer" v-else>
+    </div>
   </div>
 </template>
 
 <script>
  export default {
-   data() {
-     return {
-       play: false
-     }
-   },
    computed: {
-     workTime() {
-       return this.$store.state.workTime
+     workTimer() {
+       let minCount = Math.floor(this.$store.state.workTimer / 60);
+       let secCount = this.$store.state.workTimer % 60;
+
+       return minCount + ':' + (secCount < 10 ? '0' + secCount : secCount)
      },
-     breakTime() {
-       return this.$store.state.breakTime
+     breakTimer() {
+       let minCount = Math.floor(this.$store.state.breakTimer / 60);
+       let secCount = this.$store.state.breakTimer % 60;
+
+       return minCount + ':' + (secCount < 10 ? '0' + secCount : secCount)
      },
      isPlay() {
-       return this.play
+       return this.$store.state.isPlay
+     },
+     activeTimer() {
+       return this.$store.state.activeTimer
      }
    },
    methods: {
-     countdown() {
-       this.play = true
-       this.$store.commit('countdownWorkTime')
+     startTimer() {
+       this.$store.commit('countdown')
+     },
+     stopTimer() {
+       this.$store.commit('stopTimer')
+     },
+     activate: function(event) {
+       this.$store.commit('setActiveTimer', {
+         timer: event.target.dataset.timer
+       })
+       this.$store.dispatch('checkAudio')
+     },
+     change(event) {
+       if (event.code === 'Space') {
+         console.log('space');
+       }
      }
+   },
+   created() {
+     window.addEventListener('keyup', this.changeStatus)
    }
  }
 </script>
 
 <style>
+  html,
+  body {
+    height: 100%;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    background: #eee;
+    font-family: 'Bubblegum Sans', sans-serif;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  #app {
+    width: 500px;
+    background: #fff;
+    border-radius: 5px;
+    box-shadow: 4px 4px 10px rgba(0,0,0,.1);
+    overflow: hidden;
+    max-width: 90%;
+  }
+  header {
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: center;
+    justify-content: center;
+  }
+  .logo img {
+    width: 50px;
+    margin-right: 20px;
+  }
+  h1 {
+    font-size: 40px;
+    color: #d23834;
+    margin: 30px 0;
+  }
+  .timer-wrap {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 40px;
+  }
+  .item {
+    font-size: 30px;
+    padding: 10px 20px;
+    border: 5px solid #eee;
+    border-radius: 5px;
+    cursor: pointer;
+    margin: 0 15px 20px 15px;
+    width: 110px;
+    text-align: center;
+    color: #88c057;
+  }
+  .active {
+    border-color: #d23834;
+  }
+  .timer-button {
+    text-align: center;
+    margin-bottom: 30px;
+  }
+  .timer-button img{
+    width: 70px;
+    cursor: pointer;
+  }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -5,30 +5,106 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 Vue.config.productionTip = false
 
+// конфиг
+const config = {
+  workTimer: 24 * 60, // рабочий таймер в секундах
+  breakTimer: 5 * 60, // таймер отдыха в секундах
+  activeTimer: 'workTimer', // активный таймер по умолчанию
+  isPlay: false // флаг запуска таймера
+}
+
 const store = new Vuex.Store({
   state: {
-    workTime: 4,
-    breakTime: 5
+    workTimer: config.workTimer,
+    breakTimer: config.breakTimer,
+    activeTimer: config.activeTimer,
+    isPlay: config.isPlay,
   },
-  mutations: {
-    countdownWorkTime (state) {
-      setTimeout(function run() {
-        state.workTime -= 1
 
-        if (state.workTime === 0) {
-          store.dispatch('finish')
+  mutations: {
+    countdown (state) {
+      // проигрываем мелодию старта таймера
+      store.dispatch('startAudio')
+
+      // активируем таймер
+      state.isPlay = true
+
+      setTimeout(function run() {
+        // проверяем активен ли таймер
+        // используется при остановке таймера кнопкой СТОП
+        if (state.isPlay === false) {
+          return false;
+        }
+
+        // уменьшаем значение таймера
+        state[state.activeTimer] -= 1
+
+        // таймер подошел к концу
+        if (state[state.activeTimer] === 0) {
+          // отключаем таймер
+          state.isPlay = false
+
+          // сбрасываем все значения
+          store.commit('reset')
+
+          // переключаем активный таймер
+          store.commit('setActiveTimer', {
+            timer: state.activeTimer === 'breakTimer' ? 'workTimer' : 'breakTimer'
+          })
+
+          // проигрываем мелодию при завершении таймера
+          store.dispatch('finishAudio')
         } else {
           setTimeout(run, 1000)
         }
-      }, 1000);
+      }, 1000)
     },
-    countdownBreakTime (state) {
-      setInterval(() => state.breakTime -= 1, 1000)
+
+    // установка активного таймера
+    setActiveTimer(state, payload) {
+      state.activeTimer = payload.timer
+    },
+
+    // остановка таймера по кнопке
+    stopTimer() {
+      // сбрасываем все значения
+      store.commit('reset')
+
+      // проигрываем аудио остановки таймера
+      store.dispatch('stopAudio')
+    },
+
+    // сброс значений таймеров
+    reset(state) {
+      state.isPlay = false
+      state.workTimer = config.workTimer
+      state.breakTimer = config.breakTimer
     }
   },
+
   actions: {
-    finish() {
-      alert('hello');
+    // мелодия при завершении таймера
+    finishAudio() {
+      let audio = new Audio('/assets/sounds/finishTimer.mp3')
+      audio.play()
+    },
+
+    // мелодия при старте таймере
+    startAudio() {
+      let audio = new Audio('/assets/sounds/startTimer.mp3')
+      audio.play()
+    },
+
+    // мелодия при принудительной остановке таймера
+    stopAudio() {
+      let audio = new Audio('/assets/sounds/stopTimer.mp3')
+      audio.play()
+    },
+
+    // мелодия при смене таймера
+    checkAudio() {
+      let audio = new Audio('/assets/sounds/check.mp3')
+      audio.play()
     }
   }
 })
