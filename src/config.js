97fbfaa3ed3ece57d04ab95be4895e642176fe51
vladimir <myweb11@mail.ru>
2020-12-25 23:00:05 +0300

    добавил классный фон

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     workTimer: 25 * 60, // рабочий таймер в секундах
-    breakTimer: 5, // таймер отдыха в секундах
+    breakTimer: 5 * 60, // таймер отдыха в секундах
     currentTimer: 'workTimer', // активный таймер по умолчанию
     isPlay: false // флаг запуска таймера
 }
